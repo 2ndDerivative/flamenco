8118208053569bc182b8556fc8ac58363587b3a4
Niclas Klugmann <niclas@klugmann.de>
2026-03-02 08:12:15 +0100

    move close to separate module

--- src/file.rs
+++ src/file.rs
@@ -7,10 +7,13 @@ use std::{
 use crate::{
     ReadLe,
     error::{ErrorResponse2, ServerError},
+    file::close::{CloseRequest, CloseResponse},
     header::{Command202, SyncHeader202Outgoing},
     message::{MessageBody, Validation, read_202_message, write_202_message},
     tree::TreeConnection,
 };
+
+mod close;
 
 #[derive(Debug)]
 pub struct FileHandle<'client, 'con, 'cred, 'session, 'tree> {
@@ -356,76 +359,4 @@ enum CreateActionTaken {
 pub struct FileId {
     persistent: u64,
     volatile: u64,
-}
-
-#[derive(Clone, Copy, Debug)]
-struct CloseRequest {
-    id: FileId,
-}
-impl CloseRequest {
-    fn write_into<W: Write>(&self, mut w: W) -> Result<(), std::io::Error> {
-        w.write_all(&24u16.to_le_bytes())?;
-        w.write_all(&0u16.to_le_bytes())?;
-        w.write_all(&0u32.to_le_bytes())?;
-        let FileId {
-            persistent,
-            volatile,
-        } = self.id;
-        w.write_all(&persistent.to_le_bytes())?;
-        w.write_all(&volatile.to_le_bytes())?;
-        Ok(())
-    }
-}
-impl MessageBody for CloseRequest {
-    type Err = std::io::Error;
-    fn size_hint(&self) -> usize {
-        24
-    }
-    fn write_to<W: Write>(&self, w: W) -> Result<(), Self::Err> {
-        self.write_into(w)
-    }
-}
-
-#[derive(Clone, Debug)]
-struct CloseResponse {
-    creation_time: u64,
-    last_access_time: u64,
-    last_write_time: u64,
-    change_time: u64,
-    allocation_size: u64,
-    end_of_file: u64,
-}
-impl CloseResponse {
-    fn read_from<R: Read>(mut r: R) -> Result<Self, ReadCloseError> {
-        if r.read_u16()? != 60 {
-            return Err(ReadCloseError::InvalidStructureSize);
-        }
-        let _flags = r.read_u16()?;
-        let creation_time = r.read_u64()?;
-        let last_access_time = r.read_u64()?;
-        let last_write_time = r.read_u64()?;
-        let change_time = r.read_u64()?;
-        let allocation_size = r.read_u64()?;
-        let end_of_file = r.read_u64()?;
-        let _file_attributes = r.read_u32()?;
-        Ok(Self {
-            creation_time,
-            last_access_time,
-            last_write_time,
-            change_time,
-            allocation_size,
-            end_of_file,
-        })
-    }
-}
-
-#[derive(Debug)]
-pub enum ReadCloseError {
-    Io(std::io::Error),
-    InvalidStructureSize,
-}
-impl From<std::io::Error> for ReadCloseError {
-    fn from(value: std::io::Error) -> Self {
-        Self::Io(value)
-    }
 }
